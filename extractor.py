045b12f5632281b86c7f913c4638b58f9d3a2b0a
My Hoang <hoangquocmy@gmail.com>
2019-02-21 18:50:58 +0700

    Console Output
    Display list into console

--- extractor.py
+++ extractor.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup as bs
 
-htmlFile = bs(open("NetflixMyListExtract.html"), "html.parser")
+htmlFile = bs(open("NetflixMyListExtract.html", encoding="utf8"), "html.parser")
 
+listFilm = []
 for filmName in htmlFile.find_all("div", {"class": "fallback-text"}):
-    print(filmName.text)
+    listFilm.append(filmName.text)
+listFilm.sort()
+for filmName in listFilm:
+    print(filmName)
